3d8cf84c6c81735f2ed83d30c0e87fe06ce610b9
magnusm18 <magnusm18@ru.is>
2020-03-10 19:28:33 +0000

    6th commit, 100% green

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,12 @@
 def FizzBuzz(number):
+    
+    if number % 3 == 0 and number % 5 == 0:
+        return(print("FizzBuzz"))
+
     if number % 5 == 0:
         return(print("Buzz"))
 
     if number % 3 == 0:
         return(print("Fizz"))
 
-    if number % 3 == 0 and number % 5 == 0:
-        return(print("FizzBuzz"))
-
-FizzBuzz(15)
+FizzBuzz(6)
